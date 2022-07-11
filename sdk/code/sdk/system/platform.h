94aa3c83b6da16fe22684aab5a38a152cb2c3539
weili.hu <weili.hu@ihoment.com>
2022-07-11 11:22:54 +0800

    reset platform.h

--- sdk/code/sdk/system/platform.h
+++ sdk/code/sdk/system/platform.h
@@ -4,7 +4,7 @@
 #define PLATFORM_FR8016HA               0x801601
 #define PLATFORM_FR5089D2               0x508902
 
-#define PLATFORM_TYPE_ID                PLATFORM_FR8016HA
-#define IMAGE_APP_SIZE_MAX              0x3A000
+#define PLATFORM_TYPE_ID                ID_DEFAULT
+#define IMAGE_APP_SIZE_MAX              SIZE_DEFAULT
 
 #endif
